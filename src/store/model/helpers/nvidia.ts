8c36c266912894da3b39ca149295c6b758eed16e
Csaba Vegh <55585710+bendusz@users.noreply.github.com>
2020-12-03 01:11:02 +0000

    restore

--- src/store/model/helpers/nvidia.ts
+++ src/store/model/helpers/nvidia.ts
@@ -1,103 +1,133 @@
-import {NvidiaRegionInfo, regionInfos} from '../nvidia-api';
-import {Browser} from 'puppeteer';
-import {Link} from '../store';
-import {NvidiaCart} from './nvidia-cart';
-import {config} from '../../../config';
-import {timestampUrlParameter} from '../../timestamp-url-parameter';
+import {Link, Model, Series} from '../store';
+import {logger} from '../../../logger';
 
-function getRegionInfo(): NvidiaRegionInfo {
-	let country = config.store.country.toLowerCase();
-	if (!regionInfos.has(country)) {
-		country = 'usa';
-	}
-
-	const regionInfo = regionInfos.get(country);
-	if (!regionInfo) {
-		throw new Error(
-			`LogicException could not retrieve region info for ${country}`
-		);
-	}
-
-	return regionInfo;
+export interface Card {
+	brand: string;
+	model: Model;
 }
 
-function nvidiaStockUrl(
-	id: number,
-	drLocale: string,
-	currency: string
-): string {
-	return (
-		`https://api-prod.nvidia.com/direct-sales-shop/DR/products/${drLocale}/${currency}/${id}?` +
-		timestampUrlParameter().slice(1)
-	);
+interface LinksBuilderOptions {
+	productsSelector: string;
+	sitePrefix: string;
+	titleAttribute?: string;
+	titleSelector: string;
+	urlSelector?: string;
 }
 
-let cart: NvidiaCart;
+const isPartialUrlRegExp = /^\/[^/]/i;
 
-export function generateSetupAction() {
-	return async (browser: Browser) => {
-		cart = new NvidiaCart(browser);
+export function getProductLinksBuilder(options: LinksBuilderOptions) {
+	/* eslint-disable unicorn/no-fn-reference-in-iterator */
+	return (docElement: cheerio.Cheerio, series: Series): Link[] => {
+		const productElements = docElement.find(options.productsSelector);
+		const links: Link[] = [];
+		for (let i = 0; i < productElements.length; i++) {
+			const productElement = productElements.eq(i);
+			const titleElement = productElement.find(options.titleSelector).first();
 
-		if (config.browser.open) {
-			cart.keepAlive();
+			const title = options.titleAttribute
+				? titleElement.attr()?.[options.titleAttribute]
+				: titleElement.text()?.replace(/\n/g, ' ').trim();
+
+			if (!title) {
+				continue;
+			}
+
+			let urlElement = titleElement;
+
+			if (options.urlSelector) {
+				urlElement = urlElement.find(options.urlSelector).first();
+			}
+
+			let url = urlElement.attr()?.href;
+
+			if (!url) {
+				continue;
+			}
+
+			if (isPartialUrlRegExp.exec(url)) {
+				url = options.sitePrefix + url;
+			}
+
+			const card = parseCard(title);
+
+			if (card) {
+				links.push({
+					brand: card.brand as any,
+					model: card.model,
+					series,
+					url
+				});
+			} else {
+				logger.error(`Failed to parse card: ${title}`, {url});
+			}
 		}
+
+		return links;
 	};
+	/* eslint-enable unicorn/no-fn-reference-in-iterator */
 }
 
-export function generateOpenCartAction(id: number, cardName: string) {
-	return async () => {
-		const url = await cart.addToCard(id, cardName);
+export function parseCard(name: string): Card | null {
+	name = name.replace(/\w+-\w+-[^ ]+/g, '');
+	name = name.replace(/\([^(]*\)/g, '');
+	name = name.replace(/, .+$/, '');
+	name = name.replace(/ with .+$/, '');
 
-		return url;
+	// Account for incorrect titles, e.g. MSIGeforce
+	name = name.replace(/geforce/i, '');
+
+	name = name.replace(/[^\w ]+/g, '');
+	name = name.replace(/\bgraphics card\b/gi, '');
+	name = name.replace(/\b(?<!founders) edition\b/gi, '');
+	name = name.replace(/\b(series )?bundle\b/gi, '');
+	name = name.replace(/\b\w+ fan\b/gi, '');
+	name = name.replace(/\s{2,}/g, ' ').trim();
+
+	let model = name.split(' ');
+	const brand = model.shift();
+
+	if (!brand) {
+		return null;
+	}
+
+	// Split non spaced TitleCase words only after extracting brand
+	model = model
+		.join(' ')
+		.replace(/([A-Z][a-z]+)([A-Z][a-z]+)/g, '$1 $2')
+		.split(' ');
+
+	// Some vendors have oc at the beginning of the product name,
+	// store whether the card contains the term "oc" and remove
+	// it during filtering, then add it to the end of the name.
+	let isOC = false;
+
+	/* eslint-disable @typescript-eslint/prefer-regexp-exec */
+	model = model.filter((word) => {
+		if (word.toLowerCase() === 'oc') {
+			isOC = true;
+			return false;
+		}
+
+		return (
+			!word.match(
+				/^(nvidia|geforce|ge|force|rtx|amp[ae]re|graphics|card|gpu|pci-?e(xpress)?|ray-?tracing|ray|tracing|core|boost|epicx)$/i
+			) &&
+			!word.match(/^(\d+(?:gb?|mhz)?|gb|mhz|g?ddr(\d+x?)?)$/i) &&
+			!word.match(/^(display ?port|hdmi|vga)$/i)
+		);
+	});
+	/* eslint-enable @typescript-eslint/prefer-regexp-exec */
+
+	if (isOC) model.push('oc');
+	if (model.length === 0) return null;
+
+	return {
+		brand: brand.toLowerCase(),
+		model: model
+			.join(' ')
+			.toLowerCase()
+			.replace(/ gaming\b/g, '')
+			.trim() as Model
 	};
-}
-
-export function generateLinks(): Link[] {
-	const {
-		drLocale,
-		fe3080Id,
-		fe3090Id,
-		fe2060SuperId,
-		currency
-	} = getRegionInfo();
-
-	const links: Link[] = [];
-
-	if (fe2060SuperId) {
-		links.push({
-			brand: 'test:brand',
-			model: 'test:model',
-			openCartAction: generateOpenCartAction(fe2060SuperId, 'TEST CARD debug'),
-			series: 'test:series',
-			url: nvidiaStockUrl(fe2060SuperId, drLocale, currency)
-		});
-	}
-
-	if (fe3080Id) {
-		links.push({
-			brand: 'nvidia',
-			model: 'founders edition',
-			openCartAction: generateOpenCartAction(
-				fe3080Id,
-				'nvidia founders edition 3080'
-			),
-			series: '3080',
-			url: nvidiaStockUrl(fe3080Id, drLocale, currency)
-		});
-	}
-
-	if (fe3090Id) {
-		links.push({
-			brand: 'nvidia',
-			model: 'founders edition',
-			openCartAction: generateOpenCartAction(
-				fe3090Id,
-				'nvidia founders edition 3090'
-			),
-			series: '3090',
-			url: nvidiaStockUrl(fe3090Id, drLocale, currency)
-		});
-	}
-
-	return links;
 }
