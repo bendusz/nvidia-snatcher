0d5fda69911fce97ac58ebb152fdf37e03e66423
Csaba Vegh <55585710+bendusz@users.noreply.github.com>
2020-12-08 21:41:53 +0000

    Update discord.ts

--- src/notification/discord.ts
+++ src/notification/discord.ts
@@ -27,17 +27,17 @@ export function sendDiscordMessage(link: Link, store: Store) {
 			try {
 				const embed = new Discord.MessageEmbed()
 					.setTitle('_**Stock alert!**_')
-					.setDescription(
-						'> provided by [streetmerchant](https://github.com/jef/streetmerchant) with :heart:'
-					)
-					.setThumbnail(
-						'https://raw.githubusercontent.com/jef/streetmerchant/main/media/streetmerchant-square.png'
-					)
+					// .setDescription(
+					// 	'> provided by [streetmerchant](https://github.com/jef/streetmerchant) with :heart:'
+					// )
+					// .setThumbnail(
+					// 	'https://raw.githubusercontent.com/jef/streetmerchant/main/media/streetmerchant-square.png'
+					// )
 					.setColor('#52b788')
 					.setTimestamp();
 
 				embed.addField('Store', store.name, true);
-				if (link.price) embed.addField('Price', `$${link.price}`, true);
+				if (link.price) embed.addField('Price', `£${link.price}`, true);
 				embed.addField('Product Page', link.url);
 				if (link.cartUrl) embed.addField('Add to Cart', link.cartUrl);
 				embed.addField('Brand', link.brand, true);
