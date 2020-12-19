f7bacd712c8556667cf16f23f02676688f9a637b
Csaba Vegh <55585710+bendusz@users.noreply.github.com>
2020-12-19 22:14:18 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -181,7 +181,7 @@ const browser = {
 		5000
 	),
 	open: envOrBoolean(process.env.OPEN_BROWSER),
-	userAgent: ''
+	userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 };
 
 const docker = envOrBoolean(process.env.DOCKER, false);
